3fd8ff1eeccb55d8dd39b70ef878938ff5e4ca61
pytest <pytest514@gmail.com>
2023-08-04 17:14:05 +0800

    fix: fix wrong path

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import GlobalStyles from "./GlobalStyles/globalStyles.js";
+import GlobalStyles from "./globalstyles/globalStyles.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
