b541800e01588939d75eafe04804ee7984f27b60
pytest <pytest514@gmail.com>
2023-08-16 23:13:13 +0800

    chore: remove unsesd code

--- src/pages/HostRoomPhotos.jsx
+++ src/pages/HostRoomPhotos.jsx
@@ -1,11 +1,6 @@
-import { useOutletContext, useParams } from "react-router-dom";
-import { hostData } from "../mockdata/hostData";
+import { useOutletContext } from "react-router-dom";
 
 export function HostRoomPhotos() {
-  // const { id } = useParams();
-  // const roomData = hostData.filter((i) => i.id.toString() === id);
-  // const { imageUrl } = roomData[0];
-
   const { imageUrl } = useOutletContext();
 
   return <img src={`/images/${imageUrl}`}></img>;
